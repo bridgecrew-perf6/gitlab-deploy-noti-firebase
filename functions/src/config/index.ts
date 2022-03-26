573ef4bc8b0f2ca7f51d47c98d01906081f3d19c
pharanyu <pharanyu.c@codium.co>
2022-03-26 23:49:15 +0700

    set hangout web hook to env

--- functions/src/config/index.ts
+++ functions/src/config/index.ts
@@ -3,10 +3,6 @@ import * as firebaseFunctions from "firebase-functions";
 const firebaseConfig = firebaseFunctions.config();
 
 export default {
-  app: {
-    someKey: firebaseConfig.env.SOME_KEY || "no key",
-  },
-
   hangout: {
     web_hook: firebaseConfig.env.HANGOUT_WEB_HOOK,
   },
